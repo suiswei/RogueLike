50baa0b0e685a39af844891df8de72a2554f7012
suiswei <janbrian630@gmail.com>
2026-05-03 21:56:44 +0800

    Add enemy movement ai

---
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform player;
+    public float moveSpeed;
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);
+    }
+}
