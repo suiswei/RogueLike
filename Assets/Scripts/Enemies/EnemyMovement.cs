2b46cc9d5c7dc3c6abd3f92741a478797c1b6250
suiswei <janbrian630@gmail.com>
2026-05-30 11:23:19 +0800

    New enemies added

--- Assets/Scripts/Enemies/EnemyMovement.cs
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -7,15 +7,35 @@ public class EnemyMovement : MonoBehaviour
     EnemyStats enemy;
     Transform player;
 
+    [Header("Separation")]
+    public float separationRadius = 1.5f;
+    public float separationForce = 2f;
+
     void Start()
     {
         enemy = GetComponent<EnemyStats>();
         player = FindObjectOfType<PlayerMovement>().transform;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        Vector2 direction = player.transform.position - transform.position;
+
         transform.position = Vector2.MoveTowards(transform.position, player.transform.position, enemy.currentMoveSpeed * Time.deltaTime);
+
+        Collider2D[] neighbors = Physics2D.OverlapCircleAll(transform.position, separationRadius);
+        foreach (Collider2D neighbor in neighbors)
+        {
+            if (neighbor != null && neighbor.gameObject != gameObject && neighbor.CompareTag("Enemy"))
+            {
+                Vector2 pushDir = transform.position - neighbor.transform.position;
+                transform.position += (Vector3)(pushDir.normalized * separationForce * Time.deltaTime);
+            }
+        }
+
+        if (direction.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+        else if (direction.x > 0)
+            transform.localScale = new Vector3(1, 1, 1);
     }
 }
